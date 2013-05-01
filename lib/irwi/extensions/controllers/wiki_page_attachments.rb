07258168ef01682b41464d392fd35f225b7207ef
dandrews <daniel.raymond.andrews@gmail.com>
2013-04-30 23:27:39 -0600

    undid change to function in wiki_page_attachment

--- lib/irwi/extensions/controllers/wiki_page_attachments.rb
+++ lib/irwi/extensions/controllers/wiki_page_attachments.rb
@@ -15,7 +15,7 @@ module Irwi::Extensions::Controllers::WikiPageAttachments
   protected
 
   def page_attachment_class
-    Irwi.config.page_attachment_class = 'WikiPageAttachment'
+    Irwi.config.page_attachment_class
   end
 
 end
